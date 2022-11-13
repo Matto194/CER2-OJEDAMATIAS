1d659e9c895cac0ead19330cf13262323e9735f5
Matto194 <matias.ojedas@usm.cl>
2022-11-13 02:20:09 -0300

    commit_bd_admin

--- ProyectoCER2/core/models.py
+++ ProyectoCER2/core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Residencia(models.Model):
+    numero_residencia = models.IntegerField(max_length=3)
+    nombre_residente = models.CharField(max_length=20)
+    apellido_residente = models.CharField(max_length=20)
+
+class Correspondencia(models.Model):
+    entregado = models.BooleanField()
+    id_resi = models.ForeignKey('Residencia', on_delete=models.CASCADE)
+
+
